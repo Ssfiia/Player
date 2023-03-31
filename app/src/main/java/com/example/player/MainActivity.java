09f0616f7074c3e2740733a0fdfec3a7281c826b
Сафия <124502236+Ssfiia@users.noreply.github.com>
2023-03-31 21:52:01 +0500

    Initial commit

---
+++ app/src/main/java/com/example/player/MainActivity.java
@@ -0,0 +1,172 @@
+package com.example.player;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.annotation.SuppressLint;
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+
+public class MainActivity extends AppCompatActivity implements Runnable {
+
+    private MediaPlayer mediaPlayer = new MediaPlayer(); // создание поля медиа-плеера
+    private SeekBar seekBar; // создание поля SeekBar
+    private boolean wasPlaying = false; // поле проигрывания аудио-файла
+    private FloatingActionButton fabPlayPause; // поле кнопки проигрывания и постановки на паузу аудиофайла
+    private TextView seekBarHint;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fabPlayPause = findViewById(R.id.fabPlayPause);
+        seekBarHint = findViewById(R.id.seekBarHint);
+        seekBar = findViewById(R.id.seekBar);
+
+        // создание слушателя нажатия кнопки fabPlayPause
+        fabPlayPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playSong(); // воспроизведение музыки
+            }
+        });
+
+        // создание слушателя изменения SeekBar
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            // метод при перетаскивании ползунка по шкале,
+            // где progress позволяет получить нове значение ползунка (позже progress назрачается длина трека в миллисекундах)
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromTouch) {
+                seekBarHint.setVisibility(View.VISIBLE); // установление видимости seekBarHint
+
+                // Math.ceil() - округление до целого в большую сторону
+                int timeTrack = (int) Math.ceil(progress/1000f); // перевод времени из миллисекунд в секунды
+
+                // вывод на экран времени отсчёта трека
+                if (timeTrack < 10) {
+                    seekBarHint.setText("00:0" + timeTrack);
+                } else if (timeTrack < 60){
+                    seekBarHint.setText("00:" + timeTrack);
+                } else if (timeTrack >= 60) {
+                    seekBarHint.setText("01:" + (timeTrack - 60));
+                }
+
+                // передвижение времени отсчёта трека
+                double percentTrack = progress / (double) seekBar.getMax(); // получение процента проигранного трека (проигранное время делится на длину трека)
+                // seekBar.getX() - начало seekBar по оси Х
+                // seekBar.getWidth() - ширина контейнера seekBar
+                // 0.92 - поправочный коэффициент (так как seekBar занимает не всю ширину своего контейнера)
+                seekBarHint.setX(seekBar.getX() + Math.round(seekBar.getWidth()*percentTrack*0.92));
+
+
+            }
+            // метод при начале перетаскивания ползунка по шкале
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                seekBarHint.setVisibility(View.INVISIBLE); // установление видимости seekBarHint
+            }
+            // метод при завершении перетаскивания ползунка по шкале
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                if (mediaPlayer != null && mediaPlayer.isPlaying()) { // если mediaPlayer не пустой и mediaPlayer воспроизводится
+                    mediaPlayer.seekTo(seekBar.getProgress()); // обновление позиции трека при изменении seekBar
+                }  else {
+                    if (mediaPlayer != null && !mediaPlayer.isPlaying()) { // если mediaPlayer не пустой и не воспроизводится
+                        mediaPlayer.seekTo(seekBar.getProgress()); // обновление позиции трека при изменении seekBar
+
+                    }
+                }
+            }
+        });
+    }
+
+    // метод запуска аудио-файла
+    public void playSong() {
+        try { // обработка исключения на случай отстутствия файла
+            if (mediaPlayer != null && mediaPlayer.isPlaying()) { // если mediaPlayer не пустой и mediaPlayer воспроизводится
+                pauseMediaPlayer(); // остановка MediaPlayer
+                wasPlaying = true; // инициализация значения запуска аудио-файла
+                fabPlayPause.setImageDrawable(ContextCompat.getDrawable(MainActivity.this, android.R.drawable.ic_media_play));
+            }
+
+            if (!wasPlaying) {
+                if (mediaPlayer == null) { // если mediaPlayer пустой
+                   // mediaPlayer = new MediaPlayer(); // то выделяется для него память
+                }
+                if ( mediaPlayer != null && !mediaPlayer.isPlaying()) { // если mediaPlayer не пустой и не воспроизводится
+                    mediaPlayer.start();
+                    new Thread(this).start();
+
+                // назначение кнопке картинки pause
+                fabPlayPause.setImageDrawable(ContextCompat.getDrawable(MainActivity.this, android.R.drawable.ic_media_pause));
+                }
+
+                // альтернативный способ считывания файла с помощью файлового дескриптора
+                AssetFileDescriptor descriptor = getAssets().openFd("george_thorogood_bad_to_the_bone_mix.mp3");
+                // запись файла в mediaPlayer, задаются параметры (путь файла, смещение относительно начала файла, длина аудио в файле)
+                mediaPlayer.setDataSource(descriptor.getFileDescriptor(), descriptor.getStartOffset(), descriptor.getLength());
+                descriptor.close(); // закрытие дескриптора
+
+                mediaPlayer.prepare(); // ассинхронная подготовка плейера к проигрыванию
+                mediaPlayer.setLooping(false); // назначение отстутствия повторов
+                seekBar.setMax(mediaPlayer.getDuration()); // ограниечение seekBar длинной трека
+
+                mediaPlayer.start(); // старт mediaPlayer
+                new Thread(this).start(); // запуск дополнительного потока
+            }
+
+            wasPlaying = false; // возврат отсутствия проигрывания mediaPlayer
+
+        } catch (Exception e) { // обработка исключения на случай отстутствия файла
+            e.printStackTrace(); // вывод в консоль сообщения отсутствия файла
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        pauseMediaPlayer();
+    }
+
+    private void pauseMediaPlayer() {
+        mediaPlayer.pause();
+        mediaPlayer.getCurrentPosition();
+
+    }
+
+
+    // метод дополнительного потока для обновления SeekBar
+    @Override
+    public void run() {
+        int currentPosition = mediaPlayer.getCurrentPosition(); // считывание текущей позиции трека
+        int total = mediaPlayer.getDuration(); // считывание длины трека
+
+        // бесконечный цикл при условии не нулевого mediaPlayer, проигрывания трека и текущей позиции трека меньше длины трека
+        while (mediaPlayer != null && mediaPlayer.isPlaying() && currentPosition < total) {
+            try {
+
+                Thread.sleep(100);
+                currentPosition = mediaPlayer.getCurrentPosition(); // обновление текущей позиции трека
+
+            } catch (InterruptedException e) { // вызывается в случае блокировки данного потока
+                e.printStackTrace();
+                return; // выброс из цикла
+            } catch (Exception e) {
+                return;
+            }
+
+            seekBar.setProgress(currentPosition); // обновление seekBar текущей позицией трека
+
+        }
+
+    }
+}
